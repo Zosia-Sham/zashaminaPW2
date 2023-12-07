bc40216d2d0f561963e11361f6d4affc8a2eb7e6
Shamina Zosya <zashamina@edu.hse.ru>
2023-12-07 20:32:31 +0300

    Initial Commit

---
+++ zashaminaPW2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  zashaminaPW2
+//
+//  Created by Mac Pro on 07.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
